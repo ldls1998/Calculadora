9df9fafbd04506cc45382d960798e6f6c8d5dca8
Leonardo Loaiza <leonardodario.loaiza@unmsm.edu.pe>
2022-09-07 22:20:51 -0500

    Función operator

--- js.js
+++ js.js
@@ -13,3 +13,14 @@ function multiply(num1, num2) {
 function divide(num1, num2) {
     return num1 / num2;
 }
+
+function operate(operator, num1, num2) {
+    if (operator === "add")
+        return add(num1, num2);
+    if (operator === "subtract")
+        return subtract(num1, num2);
+    if (operator === "multiply")
+        return multiply(num1, num2);
+    if (operator === "divide")
+        return divide(num1, num2);
+}
